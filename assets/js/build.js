33be36df9ea0d52369fe1e5a3758b776adaa6ddc
Trish Young <Trish.Young@unh.edu>
2023-11-12 10:52:10 -0500

    Issue #4. Added more styling.

--- assets/js/build.js
+++ assets/js/build.js
@@ -1,30 +1,3 @@
-// Stylesheet
-class Stylesheet extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <!--CSS Styles -->
-      <link rel="stylesheet" href="assets/css/style.css" />
-
-      <!-- Fontawesome-->
-      <script src="https://kit.fontawesome.com/e724f88ba2.js" crossorigin="anonymous"></script>
-
-      <!-- Favicons -->
-      <link rel="apple-touch-icon" sizes="180x180" href="assets/icons/apple-touch-icon.png"/>
-      <link rel="icon" type="image/png" sizes="32x32" href="assets/icons/favicon-32x32.png"/>
-
-      <!-- Bootstrap CSS -->
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-      rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-      crossorigin="anonymous" />
-
-      <!-- Animate CSS CDN -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-
-      <title>Trish Young | Front-End Web Developer | Digital Artist</title>
-    `;
-  }
-}
-
 // Header
 class Navigation extends HTMLElement {
   connectedCallback() {
@@ -34,8 +7,8 @@ class Navigation extends HTMLElement {
         <h1><a href="build.html" class="none">Trish Young</a></h1>
         <ul class="navigation">
           <li><a href="build.html" class="nav-link">About</a></li>
-          <li><a href="professional_work.html" class="nav-link">Professional</a></li>
-          <li><a href="personal_work.html" class="nav-link">Personal</a></li>
+          <li><a href="professional_work.html" class="nav-link">Code</a></li>
+          <li><a href="personal_work.html" class="nav-link">Art</a></li>
           <li><a href="#contact" class="nav-link">Contact</a></li>
         </ul>
         <button class="burger-menu" id="burger-menu">
@@ -65,15 +38,14 @@ class SocialIcons extends HTMLElement {
     this.innerHTML = `
       <!-- Social accounts - Fixed to the right -->
       <div class="socials fa-2x">
-        <a href="https://www.instagram.com/patriciawilliams.art/" target="_blank"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
-        <a href="https://www.linkedin.com/in/patriciayoung14/" target="_blank"><i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i></a> 
-        <a href="https://github.com/twilliams14" target="_blank"><i class="fa-brands fa-github" style="color: #ffffff;"></i></a>
+        <a href="https://www.instagram.com/patriciawilliams.art/" aria-label="Instagram" target="_blank"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a>
+        <a href="https://www.linkedin.com/in/patriciayoung14/" aria-label="LinkedIn" target="_blank"><i class="fa-brands fa-linkedin-in" style="color: #ffffff;"></i></a> 
+        <a href="https://github.com/twilliams14" aria-label="Github" target="_blank"><i class="fa-brands fa-github" style="color: #ffffff;"></i></a>
       </div>
     `;
   }
 }
 
-customElements.define('head-stylesheet', Stylesheet);
 customElements.define('navigation-header', Navigation);
 customElements.define('main-footer', Footer);
 customElements.define('social-icons', SocialIcons);
